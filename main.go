b444e5ae153bd518c228291059cb84ddb6e24749
ryodocx <email@ryodocx.net>
2019-08-23 02:36:25 +0900

    optimize

--- main.go
+++ main.go
@@ -5,18 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 )
+
+var (
+	MaxConcurrentThread = 8
+)
+
+func init() {
+	if i, err := strconv.Atoi(os.Getenv("MAX_CONCURRENT_THREAD")); err == nil && 0 < i {
+		MaxConcurrentThread = i
+	}
+}
 
 func main() {
 	var stdin string
 	w := csv.NewWriter(os.Stdout)
-	w.Write([]string{
+
+	var mu sync.Mutex
+	write := func(input []string) {
+		mu.Lock()
+		defer mu.Unlock()
+		w.Write(input)
+		w.Flush()
+	}
+
+	write([]string{
 		"host",
 		"error",
 		"expires_on",
 	})
-	w.Flush()
 
+	var (
+		wg = sync.WaitGroup{}
+		ch = make(chan int, MaxConcurrentThread)
+	)
 	for {
 		n, err := fmt.Scan(&stdin)
 		if n == 0 {
@@ -25,21 +49,30 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		r, err := SSLCheck(stdin)
-		if err != nil {
-			log.Printf("[error]%s --> %v\n", stdin, err)
-			w.Write([]string{
-				stdin,
-				fmt.Sprint(err),
-				"",
-			})
-		} else {
-			w.Write([]string{
-				fmt.Sprintf("%s:%s", r.Host, r.Port),
-				fmt.Sprint(r.Error),
-				fmt.Sprint(r.ExpiresOn),
-			})
-			w.Flush()
-		}
+
+		ch <- 1
+		wg.Add(1)
+		go func(input string) {
+			defer func() {
+				<-ch
+				wg.Done()
+			}()
+			r, err := SSLCheck(stdin)
+			if err != nil {
+				log.Printf("[error]%s --> %v\n", stdin, err)
+				write([]string{
+					stdin,
+					fmt.Sprint(err),
+					"",
+				})
+			} else {
+				write([]string{
+					fmt.Sprintf("%s:%s", r.Host, r.Port),
+					fmt.Sprint(r.Error),
+					fmt.Sprint(r.ExpiresOn),
+				})
+			}
+		}(stdin)
 	}
+	wg.Wait()
 }
